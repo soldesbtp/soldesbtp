29f6353cb15b4d6b7fb221aaa9a090a41f8c70e1
Adnane <adkarimail@gmail.com>
2026-07-23 00:35:44 +0200

    Redirige les domaines vercel.app vers soldesbtp.ma

--- next.config.js
+++ next.config.js
@@ -8,6 +8,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    const domainsVercel = [
+      "soldesbtp.vercel.app",
+      "soldesbtp-soldesbtp1.vercel.app",
+      "soldesbtp-git-main-soldesbtp1.vercel.app",
+    ];
+
+    return domainsVercel.map((hostname) => ({
+      source: "/:path*",
+      has: [{ type: "host", value: hostname }],
+      destination: "https://soldesbtp.ma/:path*",
+      permanent: true,
+    }));
+  },
 };
 
 module.exports = nextConfig;
